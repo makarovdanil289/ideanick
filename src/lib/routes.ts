d9df2320598f20d9ea3a44023641a95478d6db9d
Danil <makarovdanil289@gmail.com>
2025-03-24 16:23:53 +0500

    Was added routing

---
+++ src/lib/routes.ts
@@ -0,0 +1,13 @@
+const getRouteParams = <T extends Record<string, boolean>>(object: T) => {
+  return Object.keys(object).reduce((acc, key) => ({ ...acc, [key]: `:${key}` }), {}) as Record<keyof T, string>
+}
+
+export const getAllIdeasRoute = () => '/'
+
+export const viewIdeaRouteParams = getRouteParams({ ideaNick: true })
+export type viewIdeaRouteParams = typeof viewIdeaRouteParams
+export const getViewIdeaRoute = ({ ideaNick }: viewIdeaRouteParams) => `/ideas/${ideaNick}`
+
+// export const viewIdeaRouteParams = { ideaNick: ':ideaNick' }
+// export type viewIdeaRouteParams = { ideaNick: string }
+// export const getViewIdeaRoute = ({ ideaNick }: { ideaNick: string }) => `/ideas/${ideaNick}`
